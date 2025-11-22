b3c1a5588c042944f62f74c9ef283c7a335b27a3
Prathamesh Mone <muprathamesh@gmail.com>
2025-11-22 18:11:01 +0530

    updated setup file

--- setup.sh
+++ setup.sh
@@ -1,32 +1,39 @@
 #install ditg
 sudo apt install d-itg
+#install irtt
+sudo apt install irtt
+#install netserver dependency
+sudo apt install netperf
 #update and reinstall flent
 cd flent
 git checkout myflent
 git pull origin myflent
 sudo make
-sudo make install 
+sudo make install
 cd -
-#install node and build dash libraries
+#install node and npm
 cd $HOME
 sudo apt install curl
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt install nodejs
 sudo apt install npm
 cd -
-cd dash.js-master
-sudo npm install
-sudo npm run build
-cd -
-#get dash dataset
-cd assets
-if [[ ! -e bbb ]]; then
-	sudo apt-get install lftp
-	lftp -c 'mirror --parallel=10 http://ftp.itec.aau.at/datasets/DASHDataset2014/BigBuckBunny/2sec/ ;exit'
-	mv 2sec bbb
-fi
-cd -
+#install http-getter
+cd ..
+git clone git@github.com:tohojo/http-getter.git
+cd http-getter
+sudo apt install libcurl4-openssl-dev
+sudo make
+sudo make install
+cd ../EvalAQM
+#install mininet
+cd ..
+git clone git://github.com/mininet/mininet
+cd mininet
+sudo util/install.sh -a
+cd ../EvalAQM
+#install gnuplot
+sudo apt install gnuplot
 #install the project
 sudo chmod +x -R scripts
-sudo pip install -r requirements.txt
 echo "Install Completed Successfully"
